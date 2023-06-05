26e7657200ea08f6a7036c16247f8c4bb81ce01a
semsi-gungor <89967853+semsi-gungor@users.noreply.github.com>
2023-06-05 23:35:35 +0300

    1 file modified

--- src/components/UI/ModalMenu/ModalMenu.js
+++ src/components/UI/ModalMenu/ModalMenu.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import classes from './ModalMenu.module.css';
 
-export default function ModalMenu({ direction, children }) {
+export default function ModalMenu({ onClose, animate, direction, children }) {
+  let slideOut;
+  let slideIn;
+
+  if (direction === 'right') {
+    slideOut = classes.slideOutRight;
+    slideIn = classes.slideInRight;
+  } else if (direction === 'left') {
+    slideOut = classes.slideOutLeft;
+    slideIn = classes.slideInLeft;
+  }
+
   return (
     <div className={classes.container}>
-      <div className={classes.shadow}></div>
-      <div className={classes.menu}>{children}</div>
+      <div className={classes.shadow} onClick={onClose}></div>
+      <div className={`${classes.menu} ${animate ? slideOut : slideIn}`}>
+        {children}
+      </div>
     </div>
   );
 }
